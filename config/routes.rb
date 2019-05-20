76db1be76092f2763befb1bdcb50f3313d183fd9
Han Xu <hanxu27@gmail.com>
2019-05-20 13:28:48 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'coupons/index'
+
+  get 'coupons/show'
+
+  get 'coupons/new'
+
+  get 'coupons/create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -13,7 +21,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :coupons, only: [:index, :show, :new, :create]
   # Example resource route with options:
   #   resources :products do
   #     member do
